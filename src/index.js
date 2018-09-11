daf729cf472bf730f8dc5a79be83504e4abf4436
Toan Nguyen <toannguyenck5@gmail.com>
2018-09-11 20:06:27 +0700

    set interval keep awake heroku per 5 minute

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import http from "http"
+
+
+setInterval(function(){
+	http.get("http://chartbitxmen.herokuapp.com/")
+	console.log("hello")
+}, 1000 * 60 * 5)
 
 ReactDOM.render(
 	React.createElement(App),
