c9df2ba4c2f11f23287c71add57161a965573d20
toannguyenck5 <toannguyenck5@gmail.com>
2018-09-13 09:10:23 +0700

    thay doi background

--- src/components/TVChartContainer/api/index.js
+++ src/components/TVChartContainer/api/index.js
@@ -4,20 +4,20 @@ import historyProvider from './historyProvider'
 const supportedResolutions = ["D"]
 
 const config = {
-    supported_resolutions: supportedResolutions
-}; 
+	supported_resolutions: supportedResolutions
+};
 
 export default {
 	onReady: cb => {
-	// console.log('=====onReady running')	
+		// console.log('=====onReady running')	
 		setTimeout(() => cb(config), 0)
-		
+
 	},
 	searchSymbols: (userInput, exchange, symbolType, onResultReadyCallback) => {
 		// console.log('====Search Symbols running')
 
 		var crytocoin = {
-			btc : {
+			btc: {
 				symbol: "BTC",
 				full_name: "www.bitxmen.com",
 				description: "Bitcoin",
@@ -25,7 +25,7 @@ export default {
 				ticker: "BTC",
 				type: "Crypto",
 			},
-			eth : {
+			eth: {
 				symbol: "ETH",
 				full_name: "www.bitxmen.com",
 				description: "Ethereum",
@@ -33,7 +33,7 @@ export default {
 				ticker: "ETH",
 				type: "Crypto",
 			},
-			bch : {
+			bch: {
 				symbol: "BCH",
 				full_name: "www.bitxmen.com",
 				description: "Bitcoin Cash",
@@ -41,7 +41,7 @@ export default {
 				ticker: "BCH",
 				type: "Crypto",
 			},
-			doge : {
+			doge: {
 				symbol: "DOGE",
 				full_name: "www.bitxmen.com",
 				description: "Dogecoin",
@@ -49,7 +49,7 @@ export default {
 				ticker: "DOGE",
 				type: "Crypto",
 			},
-			ltc : {
+			ltc: {
 				symbol: "LTC",
 				full_name: "www.bitxmen.com",
 				description: "Litecoin",
@@ -57,7 +57,7 @@ export default {
 				ticker: "LTC",
 				type: "Crypto",
 			},
-			trx : {
+			trx: {
 				symbol: "TRX",
 				full_name: "www.bitxmen.com",
 				description: "Tron",
@@ -65,7 +65,7 @@ export default {
 				ticker: "TRX",
 				type: "Crypto",
 			},
-			usdt : {
+			usdt: {
 				symbol: "USDT",
 				full_name: "www.bitxmen.com",
 				description: "Tether",
@@ -74,35 +74,17 @@ export default {
 				type: "Crypto",
 			},
 		}
-		 onResultReadyCallback([crytocoin.btc, crytocoin.eth, crytocoin.doge, crytocoin.usdt, crytocoin.bch, crytocoin.ltc, crytocoin.trx])
+		onResultReadyCallback([
+			crytocoin.btc,
+			crytocoin.eth,
+			crytocoin.doge,
+			crytocoin.usdt,
+			crytocoin.bch,
+			crytocoin.ltc,
+			crytocoin.trx
+		])
 	},
 	resolveSymbol: (symbolName, onSymbolResolvedCallback, onResolveErrorCallback) => {
-		// expects a symbolInfo object in response
-		// console.log('======resolveSymbol running')
-		// // console.log('resolveSymbol:',{symbolName})
-		// var split_data = symbolName.split(/[:/]/)
-		// // console.log({split_data})
-		// var symbol_stub = {
-		// 	name: symbolName,
-		// 	description: '',
-		// 	type: 'crypto',
-		// 	session: '24x7',
-		// 	timezone: 'Etc/UTC',
-		// 	ticker: symbolName,
-		// 	exchange: split_data[0],
-		// 	minmov: 1,
-		// 	pricescale: 1,
-		// 	has_intraday: true,
-		// 	intraday_multipliers: ['1', '60'],
-		// 	supported_resolution:  supportedResolutions,
-		// 	volume_precision: 8,
-		// 	data_status: 'streaming',
-		// }
-
-		// if (split_data[2].match(/USD|EUR|JPY|AUD|GBP|KRW|CNY/)) {
-		// 	symbol_stub.pricescale = 100
-		// }
-
 
 		// console.log({split_data})
 		var symbol_stub = {
@@ -117,34 +99,34 @@ export default {
 			pricescale: 1,
 			has_intraday: true,
 			intraday_multipliers: ['1', '60'],
-			supported_resolution:  supportedResolutions,
+			supported_resolution: supportedResolutions,
 			volume_precision: 8,
 			data_status: 'streaming',
 		}
-		setTimeout(function() {
+		setTimeout(function () {
 			onSymbolResolvedCallback(symbol_stub)
 			// console.log('Resolving that symbol....', symbol_stub)
 		}, 0)
-		
-		
+
+
 		// onResolveErrorCallback('Not feeling it today')
 
 	},
-	getBars: function(symbolInfo, resolution, from, to, onHistoryCallback, onErrorCallback, firstDataRequest) {
+	getBars: function (symbolInfo, resolution, from, to, onHistoryCallback, onErrorCallback, firstDataRequest) {
 		// console.log('=====getBars running', symbolInfo)
 		// console.log('function args',arguments)
 		// console.log(`Requesting bars between ${new Date(from * 1000).toISOString()} and ${new Date(to * 1000).toISOString()}`)
 		historyProvider.getBars(symbolInfo, resolution, from, to, firstDataRequest)
-		.then(bars => {
-			if (bars.length) {
-				onHistoryCallback(bars, {noData: false})
-			} else {
-				onHistoryCallback(bars, {noData: true})
-			}
-		}).catch(err => {
-			console.log({err})
-			onErrorCallback(err)
-		})
+			.then(bars => {
+				if (bars.length) {
+					onHistoryCallback(bars, { noData: false })
+				} else {
+					onHistoryCallback(bars, { noData: true })
+				}
+			}).catch(err => {
+				console.log({ err })
+				onErrorCallback(err)
+			})
 
 	},
 	subscribeBars: (symbolInfo, resolution, onRealtimeCallback, subscribeUID, onResetCacheNeededCallback) => {
@@ -158,7 +140,7 @@ export default {
 		// console.log('=====calculateHistoryDepth running')
 		// while optional, this makes sure we request 24 hours of minute data at a time
 		// CryptoCompare's minute data endpoint will throw an error if we request data beyond 7 days in the past, and return no data
-		return resolution < 60 ? {resolutionBack: 'D', intervalBack: '1'} : undefined
+		return resolution < 60 ? { resolutionBack: 'D', intervalBack: '1' } : undefined
 	},
 	getMarks: (symbolInfo, startDate, endDate, onDataCallback, resolution) => {
 		//optional
--- src/index.js
+++ src/index.js
@@ -2,16 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import http from "http"
 
-//-----------------------------
-/*
-	This script below just keep heroku url not sleep
-*/
-setInterval(function(){
-	http.get("http://chartbitxmen.herokuapp.com/")
-}, 1000 * 60 * 5)
-//-----------------------------
 
 ReactDOM.render(
 	React.createElement(App),
